ae204afef1c8854a700db587ff722c8796dd1928
MaIsabelSolano <misolanob@gmail.com>
2021-05-18 14:09:24 -0600

    .

--- app/src/main/java/com/uvg/retrofit2/MainActivity.kt
+++ app/src/main/java/com/uvg/retrofit2/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.uvg.retrofit2
 
+import android.annotation.SuppressLint
+import android.app.DownloadManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import androidx.appcompat.widget.SearchView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.uvg.retrofit2.databinding.ActivityMainBinding
@@ -9,19 +14,43 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import okhttp3.Dispatcher
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: ArticleAdapter
     private val articleList = mutableListOf<Articles>()
+    private lateinit var pais:String
+    private lateinit var tipo:String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.searchNews.setOnQueryTextListener(this)
+
+        //default
+        pais = "us"
+        tipo = "general"
 
         initRecyclerView()
+        searchNew(pais,tipo)
+
+        //cambio de pais
+        binding.paisUSA.setOnClickListener {
+            pais = "us"
+            searchNew(pais,tipo)
+
+        }
+        binding.paisJP.setOnClickListener {
+            pais = "jp"
+            searchNew(pais,tipo)
+        }
+        binding.paisMX.setOnClickListener {
+            pais = "mx"
+            searchNew(pais,tipo)
+        }
     }
 
     private fun initRecyclerView(){
@@ -31,13 +60,13 @@ class MainActivity : AppCompatActivity() {
         binding.rvNews.adapter = adapter
     }
 
-    private fun searchNew(category:String){
+    private fun searchNew(country:String,category:String){
 
         val api = Retrofit2()
 
         CoroutineScope(Dispatchers.IO).launch {
 
-            val call = api.getService()?.getNewsByCategory("us",category,"4b94054dbc6b4b3b9e50d8f62cde4f6c")
+            val call = api.getService()?.getNewsByCategory(country,category,"a47b8482142b4f76a6101f9649ab28ba")
             val news: NewsResponse? = call?.body()
 
             runOnUiThread{
@@ -54,6 +83,7 @@ class MainActivity : AppCompatActivity() {
                 }else{
                     showMessage("Error en retrofit")
                 }
+                hideKeyBoard()
             }
         }
     }
@@ -61,4 +91,23 @@ class MainActivity : AppCompatActivity() {
     private fun showMessage(mensaje:String){
         Toast.makeText(this, mensaje, Toast.LENGTH_LONG).show()
     }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        if (!query.isNullOrEmpty()){
+            searchNew(pais,query.toLowerCase(Locale.ROOT))
+            tipo = query.toLowerCase(Locale.ROOT)
+        }
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return true
+    }
+
+    private fun hideKeyBoard() {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.viewRoot.windowToken, 0)
+    }
+
+
 }
